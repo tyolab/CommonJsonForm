838ce20bdeb7a1be20ef6fb9b685f26d58003b21
tyodev <deve@tyo.com.au>
2018-01-04 16:49:01 +1000

    the latest firebase library (11.8.0) got serious bugs

--- src/main/java/au/com/tyo/json/JsonFormFieldRadio.java
+++ src/main/java/au/com/tyo/json/JsonFormFieldRadio.java
@@ -1,31 +1,19 @@
 package au.com.tyo.json;
 
-import com.google.api.client.util.Key;
-
 /**
  * Created by Eric Tang (eric.tang@tyo.com.au) on 17/7/17.
  */
 
 public class JsonFormFieldRadio extends JsonFormFieldOptions {
 
-    @Key
-    public String value = "";
-
     public JsonFormFieldRadio(String key, String hint) {
         super(key, "radio", hint);
     }
 
     public void check(String check, String alt) {
-//        if (null != check && null != options)
-//            for (JsonFormFieldOption option : options) {
-//                if (check.equals(option.key)) {
-//                    value = option.key;
-//                    return;
-//                }
-//            }
         value = check;
 
-        if (null != alt && (value == null || value.length() == 0))
+        if (null != alt && (value == null || ((CharSequence) value).length() == 0))
             value = alt;
     }
 
