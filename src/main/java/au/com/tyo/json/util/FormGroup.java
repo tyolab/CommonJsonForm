b57e5e52d8de01c205cbc95f4a8b286f89a54ddb
Eric Tang <eric.tang@tyo.com.au>
2018-11-26 15:30:24 +1000

    added quick access list adapter

--- src/main/java/au/com/tyo/json/util/FormGroup.java
+++ src/main/java/au/com/tyo/json/util/FormGroup.java
@@ -1,5 +1,8 @@
 package au.com.tyo.json.util;
 
+import au.com.tyo.json.JsonFormField;
+import au.com.tyo.json.JsonFormFieldWithTitle;
+
 public class FormGroup extends FormData {
 
     public FormGroup(String title) {
@@ -21,4 +24,28 @@ public class FormGroup extends FormData {
         putInOrder(key, formField);
     }
 
+    public void addTitledJsonFormField(String title, String type) {
+        addTitledJsonFormField(title, type, null, false);
+    }
+
+    public void addTitledJsonFormField(String title, String value, String type, boolean clickable) {
+        addTitledJsonFormField(toKey(title), title, value, type, clickable);
+    }
+
+    public void addTitledJsonFormField(String key, String title, String value, String type, boolean clickable) {
+        JsonFormField jsonFormField = new JsonFormFieldWithTitle(key, title);
+        jsonFormField.type = type;
+        jsonFormField.value = value;
+        jsonFormField.clickable = clickable;
+
+        addField(key, title, jsonFormField);
+    }
+
+    public void addField(JsonFormFieldWithTitle jsonFormField) {
+        addField(jsonFormField.key, jsonFormField.title, jsonFormField);
+    }
+
+    public void addField(JsonFormField jsonFormField) {
+        addField(jsonFormField.key, toTitle(jsonFormField.key), jsonFormField);
+    }
 }
