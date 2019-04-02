a5463aa4dd46ecb2cf0fcfc7db0d3d7296477f03
Eric Tang <eric.tang@tyo.com.au>
2019-04-02 14:46:06 +1000

    * orderedmap updates * contenttypes updates

--- src/main/java/au/com/tyo/json/util/OrderedDataMap.java
+++ src/main/java/au/com/tyo/json/util/OrderedDataMap.java
@@ -1,14 +1,24 @@
 package au.com.tyo.json.util;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import au.com.tyo.json.form.DataJson;
 
-public class OrderedDataMap<ValueType> extends DataJson {
+public class OrderedDataMap<ValueType, ContainerType extends List<ValueType>> extends DataJson {
 
     private List<String>            orderedKeys;
-    private List<ValueType>         values;
+    private ContainerType           values;
+
+    /**
+     *
+     * @param key
+     * @param valueType
+     */
+    public void pushBack(String key, ValueType valueType) {
+        putInOrder(key, valueType);
+    }
 
     /**
      * The put method won't keep the entry in order
@@ -25,8 +35,9 @@ public class OrderedDataMap<ValueType> extends DataJson {
             if (!containsKey(key)) {
                 orderedKeys.add(key);
 
+                // use ArrayList as container if no container set
                 if (null == values)
-                    values = new ArrayList();
+                    values = (ContainerType) new ArrayList();
                 values.add(value);
             }
         }
@@ -62,16 +73,55 @@ public class OrderedDataMap<ValueType> extends DataJson {
         return orderedKeys.get(pos);
     }
 
-    public List getOrderedValues() {
+    public ContainerType getOrderedValues() {
         return values;
     }
 
-    public void append(OrderedDataMap<ValueType> orderedDataMap) {
+    public void append(OrderedDataMap orderedDataMap) {
         for (int i = 0; i < orderedDataMap.size(); ++i)
-            putInOrder(orderedDataMap.getKey(i), orderedDataMap.getValue(i));
+            putInOrder(orderedDataMap.getKey(i), (ValueType) orderedDataMap.getValue(i));
     }
 
-    public void putInOrder(OrderedDataMap<ValueType> orderedDataMap) {
+    public void putInOrder(OrderedDataMap orderedDataMap) {
         append(orderedDataMap);
+    }
+
+    public void setOrderContainer(ContainerType container) {
+        values = container;
+    }
+
+    public ContainerType getOrderContainer() {
+        return getValues();
+    }
+
+    public ContainerType getValues() {
+        return values;
+    }
+
+    public void removeOldest() {
+        synchronized (this) {
+            if (orderedKeys.size() == 0)
+                return;
+            String ketStr = orderedKeys.get(0);
+            remove(ketStr);
+            orderedKeys.remove(0);
+            values.remove(0);
+        }
+    }
+
+    public ValueType popOldest() {
+        ValueType value = peekOldest();
+
+        removeOldest();
+
+        return value;
+    }
+
+    public ValueType peekOldest() {
+        if (values.size() == 0)
+            return null;
+
+        ValueType value = values.get(0);
+        return value;
     }
 }
